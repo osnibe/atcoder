150eb1130a00e6c51b014bc98c9ee8f5501e77b9
osnibe <osnibe@gmail.com>
2024-09-25 23:10:47 +0900

    initial commit

---
+++ abc137_a/abc137.cpp
@@ -0,0 +1,14 @@
+// clang++ -std=c++11 abc137_a.cpp
+// https://atcoder.jp/contests/abc137/tasks/abc137_a
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin << A << B;
+
+
+
+}
