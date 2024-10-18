042a88817fdd49c58dd206e844aa12324c00a314
osnibe <osnibe@gmail.com>
2024-10-19 07:49:04 +0900

    mod abc137_a.cpp

--- abc137_a/abc137_a.cpp
+++ abc137_a/abc137_a.cpp
@@ -7,7 +7,25 @@ using namespace std;
 int main()
 {
     int A, B;
+    int add, subtract, times;
     cin >> A >> B;
 
+    add = A + B;
+//    cout << add << endl;
+    subtract = A - B;
+//    cout << subtract << endl;
+    times = A * B;
+//    cout << times << endl;
 
+    if (add > subtract)
+        if (add > times)
+            cout << add << endl;
+        else
+            cout << times << endl;
+    else if (subtract > times)
+        cout << subtract << endl;
+    else
+        cout << times << endl;
+
+    return 0; 
 }
